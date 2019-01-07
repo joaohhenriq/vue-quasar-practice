6ec9b0a25f374ac8d50ebd57b8524d74c402b62f
joaohhenriq <joao_camargo10@hotmail.com>
2019-01-06 23:49:33 -0200

    Testando classes da guia Flex CSS do quasar

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,33 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page class="container"> <!--servirá como container-->
+    <!--<img alt="Quasar logo" src="~assets/quasar-logo-full.svg">-->
+    <!--<div class="q-ml-sm q-mt-sm q-mr-sm border-black">DIV</div>-->
+    <!--sintaxe de espaçamento: https://quasar-framework.org/components/spacing.html-->
+
+    <!--Flex CSS: https://quasar-framework.org/components/flex-css.html-->
+    <div class="row justify-center gutter-xs"> <!--linha--> <!--Gutter dá espaçamento entre os botões-->
+      <div class="col-lg-3 col-xs-9" > <!--coluna--> <!--segunda classe de coluna para rodar em celular-->
+        <q-btn color="primary" class="full-width">Button</q-btn>
+      </div>
+      <div class="col-lg-3 col-xs-9"> <!--coluna-->
+        <q-btn color="primary" class="full-width">Button</q-btn>
+      </div>
+      <div class="col-lg-3 col-xs-9"> <!--coluna-->
+        <q-btn color="primary" class="full-width">Button</q-btn>
+      </div>
+
+      <!--<div class="col">
+        <q-btn color="dark" class="full-width">Button</q-btn>
+      </div>-->
+    </div>
+
   </q-page>
 </template>
 
 <style>
+.border-black {
+  border: 1px solid black;
+}
 </style>
 
 <script>
